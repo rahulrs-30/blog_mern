c527b4e2ca6a1f25bd033f1b2b56e3968ab8a7a4
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-14 01:30:32 +0530

    created new post page

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const User= require('./models/User');
 const bcrypt=require('bcryptjs');
 const cookieParser =require('cookie-parser');
 const jwt =require('jsonwebtoken');
+const multer=require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs =require('fs');
 
 const app =express();
 
@@ -67,9 +70,14 @@ app.post('/logout',(req,res)=>{
     res.cookie('token','').json('ok');
 });
 
-
-
-
+app.post('/post', uploadMiddleware.single('file'), (req,res) => {
+    const {originalname,path} =req.file;
+    const parts= originalname.split('.');
+    const ext =parts[parts.length -1];
+    const newPath= path +'.'+ ext
+    fs.renameSync(path, newPath);
+    res.json({ext});
+})
 
 
 
