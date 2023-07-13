bbc3dd55f8c3ac210a866eb58fd0643082a8f04d
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-13 14:02:58 +0530

    authentication completed

--- api/index.js
+++ api/index.js
@@ -3,13 +3,19 @@ const cors=require('cors');
 const { default: mongoose } = require('mongoose');
 const User= require('./models/User');
 const bcrypt=require('bcryptjs');
-const app= express();
+// const cookieParser=require('cookie-parser');
+const jwt =require('jsonwebtoken');
+
 
 
 const salt = bcrypt.genSaltSync(10);
+const secret='sadhbuosac12vao3';
 
-app.use(cors());
+app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
+// app.use(cookieParser());
+
+
 
 mongoose.connect('mongodb+srv://blog:rahulrs30@cluster0.zrgtxt7.mongodb.net/?retryWrites=true&w=majority');
 
@@ -26,6 +32,24 @@ app.post('/register',async(req,res)=>{
     }
     
 });
+
+app.post('/login',async(req,res)=>{
+    const{username,password}=req.body;
+    const userDoc=await User.findOne({username});
+    const passOk= bcrypt.compareSync(password,userDoc.password);
+    if(passOk){
+        jwt.sign({username,id:userDoc._id}, secret,{},(err,token)=>{
+            if(err)throw err;
+            res.cookie('token',token).json('ok');
+        });
+    }else{
+        res.status(400).json('Wrong credentials');
+    }
+});
+
+// app.get('/profile', (req,res) => {
+//     res.json(req.cookies);
+// });
 
 
 
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Link} from'react-router-dom'
 
 const Header = () => {
+    // useEffect(()=>{
+    //     fetch('http://localhost:4000/profile',{
+    //         credentials: 'include',
+    //     })
+    // },[]);
     return (
         <header>
             <Link to='/' className='logo'>
