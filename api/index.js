b79cfa17a8c51f37e5367d09c1bb24c8c0795ea6
Rahul Raj Singh <singhrahulraj.2001@gmail.com>
2023-07-14 16:16:29 +0530

    displaying post from db

--- api/index.js
+++ api/index.js
@@ -1,93 +1,110 @@
-const express= require('express');
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
 const { default: mongoose } = require('mongoose');
-const User= require('./models/User');
-const Post =require('./models/Post');
-const bcrypt=require('bcryptjs');
-const cookieParser =require('cookie-parser');
-const jwt =require('jsonwebtoken');
-const multer=require('multer');
+const User = require('./models/User');
+const Post = require('./models/Post');
+const bcrypt = require('bcryptjs');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
-const fs =require('fs');
+const fs = require('fs');
 
-const app =express();
+
+const app = express();
 
 
 
 const salt = bcrypt.genSaltSync(10);
-const secret='sadhbuosac12vao3';
+const secret = 'sadhbuosac12vao3';
 
-app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 
 
 
 mongoose.connect('mongodb+srv://blog:rahulrs30@cluster0.zrgtxt7.mongodb.net/?retryWrites=true&w=majority');
 
-app.post('/register',async(req,res)=>{
-    const {username,password}=req.body;
-    try{
-        const userDoc= await User.create({
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userDoc = await User.create({
             username,
-            password: bcrypt.hashSync(password,salt),
+            password: bcrypt.hashSync(password, salt),
         });
         res.json(userDoc);
-    }catch(e){
+    } catch (e) {
         res.status(400).json(e);
     }
-    
+
 });
 
-app.post('/login',async(req,res)=>{
-    const{username,password}=req.body;
-    const userDoc=await User.findOne({username});
-    const passOk= bcrypt.compareSync(password,userDoc.password);
-    if(passOk){
-        jwt.sign({username,id:userDoc._id}, secret,{},(err,token)=>{
-            if(err)throw err;
-            res.cookie('token',token).json({
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
                 id: userDoc._id,
                 username,
             });
         });
-    }else{
+    } else {
         res.status(400).json('Wrong credentials');
     }
 });
 
-app.get('/profile', (req,res)=>{
-    const {token}=req.cookies;
-    jwt.verify(token, secret, {}, (err,info)=>{
-        if(err)throw err;
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err;
 
         res.json(info);
     });
-    
+
 });
 
-app.post('/logout',(req,res)=>{
-    res.cookie('token','').json('ok');
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadMiddleware.single('file'), async(req,res) => {
-    const {originalname,path} =req.file;
-    const parts= originalname.split('.');
-    const ext =parts[parts.length -1];
-    const newPath= path +'.'+ ext
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext
     fs.renameSync(path, newPath);
-    const{title,summary,content}=req.body;
-    const postDoc= await Post.create({
-        title,
-        summary,
-        content,
-        cover: newPath,
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async(err, info) => {
+        if (err) throw err;
+
+        const { title, summary, content } = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id,
     });
-
-
     res.json(postDoc);
+    });    
 });
+
+
+app.get('/post', async (req, res) => {
+    res.json(
+        await Post.find()
+        .populate('author', ['username'])
+        .sort({createdAt: -1})
+        .limit(20)
+        );
+})
 
 
 
--- client/src/post.js
+++ client/src/post.js
@@ -1,29 +1,26 @@
 import React from 'react';
+import {format} from 'date-fns';
 
-const Post=()=>{
+export default function Post({title,summary,content,cover,createdAt,author}){
     return (
         <div className='post'>
             <div className='image'>
                 <img
-                    src="https://helios-i.mashable.com/imagery/articles/07bbbVMOuthy4RtLmDUrMjN/hero-image.fill.size_1248x702.v1614274899.png"
+                    src={'http://localhost:4000/'+cover}
                     alt=""
                 />
             </div>
             <div className='texts'>
-                <h2>PS5 Just Did Something No Console Ever Has</h2>
+                <h2>{title}</h2>
                 <p className='info'>
-                    <a className='author'>RRS</a>
-                    <time>12-07-2023 18:20</time>
+                    <a className='author'>{author.username}</a>
+                    <time>{format(new Date(createdAt),'MMM d, yyyy HH:mm')}</time>
                 </p>
-                <p>
-                    Sony's latest earnings report has arrived, and in it are new sales
-                    numbers for the PlayStation 5. In short, the console is performing
-                    exceptionally well. The system shipped another 6.3 million units
-                    during the January-March period to push the PS5's total to 38.4
-                    million to date.
+                <p className='summary'>
+                    {summary}
                 </p>
             </div>
         </div>
     );
 }
-export default Post;
+
